b6de4e75b5139af351ede196ca6c8fdfafe02713
Collin Bachman <collin@bachman.io>
2022-01-29 18:44:20 -0500

    Add Privacy Policy Cypress test

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,8 +1,14 @@
-describe('Contentful Page', () => {
+describe('Contentful Page Dynamic Route', () => {
     it('should preview a draft Contentful Page', () => {
         cy.visit('/api/preview/page?secret=' + Cypress.env('PREVIEW_SECRET') + '&slug=forever-a-draft');
         cy.contains('Preview Mode');
         cy.contains('Heading 1 Test');
         cy.contains('Paragraph below heading.');
+    });
+    it('should have rendered a Privacy Policy', () => {
+        cy.visit('/privacy');
+        cy.contains('Preview Mode').should('not.exist');
+        cy.contains('Privacy Policy');
+        cy.contains('By email');
     });
 });
