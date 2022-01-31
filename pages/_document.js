7397166ea842ea1dc8f65093dc3b9f00256e3383
Collin Bachman <collin@bachman.io>
2022-01-31 11:46:48 -0500

    Use Next.js Font Optimization

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,13 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                            <link
+                                href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@400;700&display=swap"
+                                rel="stylesheet" />
+                </Head>
                 <body className="bg-blue-galaxy">
                 <Main />
                 <NextScript />
