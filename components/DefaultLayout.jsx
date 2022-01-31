e13fa412f5895edd1c0bce7989e68d15880fc29a
Collin Bachman <collin@bachman.io>
2022-01-31 11:47:32 -0500

    Adjust max width of inner container

--- components/DefaultLayout.jsx
+++ components/DefaultLayout.jsx
@@ -6,7 +6,7 @@ export default function DefaultLayout({ children,}) {
         <>
             <Navigation />
             <main className="max-w-7xl mx-auto px-4 py-5 sm:px-6 lg:px-8">
-                <div className="max-w-4xl mx-auto mt-16">
+                <div className="max-w-6xl mx-auto mt-16">
                     {children}
                     <Footer />
                 </div>
