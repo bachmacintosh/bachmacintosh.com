3f31d1b4e139d3ff418caaf7a2def3fcd8282e1b
Collin Bachman <collin@bachman.io>
2022-01-31 11:57:26 -0500

    Add tooltip with meanings to WaniKani Subject component

--- components/wanikani/WanikaniSubject.jsx
+++ components/wanikani/WanikaniSubject.jsx
@@ -1,4 +1,21 @@
-export default function WanikaniSubject({subjectType, href, children,}) {
+import {createRef, useState,} from "react";
+import {createPopper,} from "@popperjs/core";
+
+export default function WanikaniSubject({subjectType, meanings, href, children,}) {
+    const [tooltipShow, setTooltipShow,] = useState(false);
+    const btnRef = createRef();
+    const tooltipRef = createRef();
+
+    const openLeftTooltip = () => {
+        createPopper(btnRef.current, tooltipRef.current, {
+            placement: "top",
+        });
+        setTooltipShow(true);
+    };
+    const closeLeftTooltip = () => {
+        setTooltipShow(false);
+    };
+
     let color = 'gray-500';
     if(subjectType === 'radical') {
         color = 'bg-wanikani-radical';
@@ -8,10 +25,32 @@ export default function WanikaniSubject({subjectType, href, children,}) {
         color = 'bg-wanikani-vocabulary';
     }
     return(
-        <div className={"w-auto mx-1 my-1 " + color}>
-            <a className={"w-auto p-1 text-xs text-white hover:underline " + color} href={href} target="_blank" rel="nofollow noreferrer noopener">
-                {children}
-            </a>
-        </div>
+        <>
+            <div className="flex flex-wrap">
+                <div className="text-center">
+                    <a className={color + " text-white font-bold text-sm px-1 py-1 shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"}
+                       type="button"
+                       onMouseEnter={openLeftTooltip}
+                       onMouseLeave={closeLeftTooltip}
+                       ref={btnRef}
+                       href={href} target="_blank" rel="nofollow noreferrer noopener"
+                       style={{fontFamily: 'Noto Sans JP',}}
+                    >
+                        {children}
+                    </a>
+                    <div
+                        className={(tooltipShow ? "" : "hidden ") + "h-auto border-0 inline z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words"}
+                        ref={tooltipRef}
+                    >
+                        <div>
+                            <div className="text-white p-3 bg-gray-500">
+                                {meanings}
+                            </div>
+                        </div>
+                        <div className="w-0 h-0 border-4 border-transparent" />
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
