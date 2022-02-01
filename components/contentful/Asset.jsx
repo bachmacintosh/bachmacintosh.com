7cb045d60eef5f69d37665dd2b26822b5d12cb50
Collin Bachman <collin@bachman.io>
2022-02-01 17:55:14 -0500

    [Lint Fix] Arrow function single return in one line

--- components/contentful/Asset.jsx
+++ components/contentful/Asset.jsx
@@ -2,9 +2,7 @@ import Image from 'next/image';
 
 export default function Asset({id, assets,},) {
     const asset = assets?.find((asset,) => asset.sys.id === id,);
-    const contentfulLoader = ({ src, width, quality, },) => {
-        return `${src}?w=${width}&q=${quality || 75}`;
-    };
+    const contentfulLoader = ({ src, width, quality, },) => `${src}?w=${width}&q=${quality || 75}`;
     if(asset?.width && asset?.height) {
         return(
             <figure>
