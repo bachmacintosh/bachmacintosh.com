0bf430c30313292a9485ae5488cf749738dd59b5
Collin Bachman <collin@bachman.io>
2022-02-01 20:36:43 -0500

    Don't destructure the query in api route

--- pages/api/preview/page.js
+++ pages/api/preview/page.js
@@ -1,12 +1,11 @@
 import { getPage, } from '../../../lib/contentful/page';
 
 export default async function page(req, res,) {
-    const { secret, slug, } = req.query;
     const regex = /^[a-zA-Z0-9-_]+$/;
-    const found = slug.match(regex,);
+    const found = req.query.slug.match(regex,);
     const pageSlug = found.length > 0 ? found[0] : false;
 
-    if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !pageSlug) {
+    if (req.query.secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !pageSlug) {
         return res.status(401,).json({ message: 'Invalid token', },);
     }
     const page = await getPage(pageSlug, true,);
