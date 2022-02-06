63cd501bd8392568ec9faf8850d63291dcafef51
Collin Bachman <collin@bachman.io>
2022-02-06 16:17:25 -0500

    Further tweak content width

--- components/DefaultLayout.jsx
+++ components/DefaultLayout.jsx
@@ -5,8 +5,8 @@ export default function DefaultLayout ({ children, },) {
   return (
     <>
       <Navigation />
-      <main className="max-w-7xl mx-auto px-4 py-5 sm:px-6 lg:px-8">
-        <div className="max-w-6xl mx-auto mt-16">
+      <main className="max-w-8xl mx-auto px-4 py-5 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto mt-16">
           {children}
           <Footer />
         </div>
