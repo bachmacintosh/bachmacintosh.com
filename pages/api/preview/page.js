c0a220c6f3bc56acb2c1f816dc0bd1bc8fa345e6
Collin Bachman <collin@bachman.io>
2022-02-07 17:59:05 -0500

    Check for secret/slug first before checking Contentful

--- pages/api/preview/page.js
+++ pages/api/preview/page.js
@@ -1,15 +1,15 @@
 import { getPreviewPage, getPreviewSlugs, } from "../../../lib/contentful/page";
 
 export default async function page (req, res,) {
-  const slugs = await getPreviewSlugs();
-  const foundItem = slugs.find(({ slug, },) => {
-    return slug === req.query.slug;
-  }, );
-
   if (req.query.secret !== process.env.CONTENTFUL_PREVIEW_SECRET
     || !req.query.slug) {
     return res.status(401,).json({ message: "Invalid token", },);
   }
+
+  const slugs = await getPreviewSlugs();
+  const foundItem = slugs.find(({ slug, },) => {
+    return slug === req.query.slug;
+  }, );
   const pageResult = await getPreviewPage(foundItem?.slug,);
 
   if (!foundItem) {
