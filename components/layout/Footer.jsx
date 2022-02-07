44c5c35def97f8d6319708a25ac50baa5673b5a0
Collin Bachman <collin@bachman.io>
2022-02-07 15:26:34 -0500

    Give footer text an ID

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer () {
   return (
     <>
       <hr className="border-blue-diamond" />
-      <span className="text-xs text-gray-400">
+      <span id="footer" className="text-xs text-gray-400">
                         Copyright &copy;
         {new Date().getFullYear() > 2022 ? `2022-${new Date().getFullYear()}` : new Date().getFullYear()}
         {" "}
