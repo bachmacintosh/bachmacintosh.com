06d8d570a474d0ae3e26920058ee20004ae1a4c5
Collin Bachman <collin@bachman.io>
2022-02-13 20:04:53 -0500

    Adjust anime poster images to prevent table overflows

--- components/anime/AniListImage.jsx
+++ components/anime/AniListImage.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function AniListImage ({ src, alt, },) {
-  return <div className="w-24 md:w-48 align-top">
+  return <div className="w-14 md:w-28 align-top">
     <Image
       src={src} alt={alt}
       width={200}
