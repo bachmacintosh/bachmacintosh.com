a6d9d83152ac7af17315842642ea89914c40a8d5
Collin Bachman <collin@bachman.io>
2022-02-13 21:44:58 -0500

    Require custom loaders for next/image

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const dateOptions = {
 };
 module.exports = {
   reactStrictMode: true,
-  images: { domains: ["s4.anilist.co",], },
+  images: { domains: ["s4.anilist.co",], loader: "custom", },
   env: {
     version: "0.3",
     buildTime: new Date().toLocaleString("en-US", dateOptions,),
