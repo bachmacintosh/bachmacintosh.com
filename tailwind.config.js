407c7fe31f31d5eba7f50fa23aeeee56794f61ce
Collin Bachman <collin@bachman.io>
2022-02-13 19:46:52 -0500

    Install Tailwind CSS Aspect Ratio plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,26 +9,22 @@ module.exports = {
     extend: {
       colors: {
         blue: {
-          galaxy: '#000D14',
-          mariner: '#3B4E78',
-          diamond: '#95B2DB',
-          racing: '#0E316D',
-          ultra: '#0055C4',
-          standard: '#001B57',
+          galaxy: "#000D14",
+          mariner: "#3B4E78",
+          diamond: "#95B2DB",
+          racing: "#0E316D",
+          ultra: "#0055C4",
+          standard: "#001B57",
         },
         wanikani: {
-          radical: '#0093dd',
-          kanji: '#dd0093',
-          vocabulary: '#9300dd',
+          radical: "#0093dd",
+          kanji: "#dd0093",
+          vocabulary: "#9300dd",
         },
       },
-      spacing: {
-        256: '64rem',
-      },
+      spacing: { 256: "64rem", },
     },
-    fontFamily: {
-      sans: ['Noto Sans', 'Arial', 'sans-serif',],
-    },
+    fontFamily: { sans: ["Noto Sans", "Arial", "sans-serif",], },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio",),],
 };
