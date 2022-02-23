6c5a7a5e5ff821eecf4478ecfdaa097a8db05cc1
Collin Bachman <collin@bachman.io>
2022-02-22 20:53:38 -0500

    Add WebPage JSON+LD to Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import { Heading1, Paragraph, } from "../components/layout/Typography";
+import { NextSeo, WebPageJsonLd, } from "next-seo";
+import { getPageJsonLd, getPageSEO, } from "../lib/seo";
 import DefaultLayout from "../components/DefaultLayout";
-import { NextSeo, } from "next-seo";
-import { getPageSEO, } from "../lib/seo";
 import { useRouter, } from "next/router";
 
 export default function Home () {
   const title = "Home";
   const description = "The Website of Collin G. Bachman";
+  const lastreviewed = new Date(2022, 1, 23, 1, 38,);
   const router = useRouter();
   return (
     <>
       <NextSeo {...getPageSEO(title, description, router,)} />
+      <WebPageJsonLd
+        {...getPageJsonLd(title, description, lastreviewed, router,)}
+      />
       <Heading1>BachMacintosh</Heading1>
       <Paragraph>
         Something lurks around the corner… on 4/9/22
