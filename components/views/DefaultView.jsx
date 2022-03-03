8544145b690fb12bdef8944c16d4794ad4fcbf0b
Collin Bachman <collin@bachman.io>
2022-03-03 17:36:03 -0500

    Adjust breakpoints on Default View

--- components/views/DefaultView.jsx
+++ components/views/DefaultView.jsx
@@ -5,7 +5,7 @@ export default function DefaultView ({ children, },) {
   return (
     <>
       <Navigation />
-      <main className="mx-auto px-4 py-5 sm:px-6 lg:px-8">
+      <main className="mx-auto px-4 py-5 md:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto mt-16">
           {children}
           <Footer />
