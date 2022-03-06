aa31a7fe00604586d11375e6a2df1a0ecb72facd
Collin Bachman <collin@bachman.io>
2022-03-06 05:09:34 -0500

    Add warning if Deploy Preview on Netlify

--- components/views/DefaultView.jsx
+++ components/views/DefaultView.jsx
@@ -1,5 +1,6 @@
 import Footer from "../layout/Footer";
 import Navigation from "../layout/Navigation";
+import Warning from "../layout/Warning";
 
 export default function DefaultView ({ children, },) {
   return (
@@ -7,6 +8,12 @@ export default function DefaultView ({ children, },) {
       <Navigation />
       <main className="mx-auto px-4 py-5 md:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto mt-16">
+          {process.env.isDeployPreview
+            && <Warning title="Deploy Preview">
+              This may contain test data, and is not indicative of the live site
+              .
+            </Warning>
+          }
           {children}
           <Footer />
         </div>
