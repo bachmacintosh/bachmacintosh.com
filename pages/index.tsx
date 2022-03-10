946fbf0129318c428cadaec08eb43be3c59eb362
Collin Bachman <collin@bachman.io>
2022-03-10 18:29:41 -0500

    Convert Home Page to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,23 @@ import {
   Paragraph,
 } from "../components/layout/Typography";
 import { ButtonLink, } from "../components/layout/Buttons";
+import { GetStaticProps, } from "next";
 import HomeView from "../components/views/HomeView";
 import { NextSeo, } from "next-seo";
 import PostList from "../components/blog/PostList";
+import { ReactElement, } from "react";
 import YouTube from "react-youtube";
 import { getHomePageBlogPosts, } from "../lib/contentful/blogpost";
 import { getLatestYouTubeVideo, } from "../lib/google/youtube";
 import { getPageSEO, } from "../lib/seo";
 import { useRouter, } from "next/router";
 
-export default function Home ({ posts, youTubeVideoId, },) {
+type pageProps = {
+    posts: Array<object>,
+    youTubeVideoId: string,
+};
+
+const Home = ({ posts, youTubeVideoId, }: pageProps,) => {
   const title = "Home";
   const description = "The Website of Collin G. Bachman";
   const router = useRouter();
@@ -26,7 +33,7 @@ export default function Home ({ posts, youTubeVideoId, },) {
       <Heading1>I Write Blogs</Heading1>
       { posts.length > 0
         && <>
-          <Paragraph>
+          <Paragraph indent={false}>
             Here&apos;s the latest one.
           </Paragraph>
           <PostList posts={posts} />
@@ -39,13 +46,13 @@ export default function Home ({ posts, youTubeVideoId, },) {
       }
       { posts.length === 0
         && <>
-          <Paragraph>
+          <Paragraph indent={false}>
             ...I mean there&apos;s nothing there yet, but there will be soon.
           </Paragraph>
         </>
       }
       <Heading1>I Make Videos</Heading1>
-      <Paragraph>
+      <Paragraph indent={false}>
         Mostly just gaming videos, but maybe some music in the future...
       </Paragraph>
       <div style={{ width: 400, height: 225, }}>
@@ -60,7 +67,7 @@ export default function Home ({ posts, youTubeVideoId, },) {
         More on YouTube
       </ButtonLink>
       <Heading1>I Play GTA Online</Heading1>
-      <Paragraph>
+      <Paragraph indent={false}>
         Party Cannon Inc. &mdash; Making Los Santos, San Andreas a better place
         {" "}
         (to die) since 2016
@@ -71,7 +78,9 @@ export default function Home ({ posts, youTubeVideoId, },) {
         GTA Stats
       </ButtonLink>
       <Heading1>I Like Japan</Heading1>
-      <Paragraph>Whether it&apos;s the culture or the language...</Paragraph>
+      <Paragraph indent={false}>
+          Whether it&apos;s the culture or the language...
+      </Paragraph>
       <ButtonLink
         href="/jpn/anime"
         external={false}>
@@ -84,9 +93,9 @@ export default function Home ({ posts, youTubeVideoId, },) {
       </ButtonLink>
     </>
   );
-}
+};
 
-Home.getView = function getView (page,) {
+Home.getView = (page: ReactElement,) => {
   return (
     <HomeView>
       {page}
@@ -94,7 +103,9 @@ Home.getView = function getView (page,) {
   );
 };
 
-export async function getStaticProps () {
+export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
   let posts = await getHomePageBlogPosts();
   if (typeof posts === "undefined") {
     posts = null;
@@ -104,6 +115,6 @@ export async function getStaticProps () {
     posts,
     youTubeVideoId,
   };
-  
+
   return { props, };
-}
+};
