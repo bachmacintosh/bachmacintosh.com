b41ccceeeff35a5b04822cd45f651fef194c8fe2
Collin Bachman <collin@bachman.io>
2022-03-16 20:16:01 -0400

    Converted remaining static pages to TypeScript

--- pages/gaming/index.tsx
+++ pages/gaming/index.tsx
@@ -4,6 +4,7 @@ import {
   Paragraph,
 } from "../../components/layout/Typography";
 import DefaultView from "../../components/views/DefaultView";
+import { ReactElement, } from "react";
 import { getPageSEO, } from "../../lib/seo";
 import { useRouter, } from "next/router";
 
@@ -31,18 +32,18 @@ export default function Gaming () {
       <Heading1>
         Gaming
       </Heading1>
-      <Paragraph>
+      <Paragraph indent={false}>
         You seem to be interested in my gaming stuff. I mostly play
         {" "}
         single-player games in my free time, but do enjoy playing with friends
         {" "}
         on the weekends. Below are some sub-pages that go into greater detail.
       </Paragraph>
-      <Paragraph>
+      <Paragraph indent={false}>
         This page is a stub; I hope to improve on its detail someday...
       </Paragraph>
       <Heading2>GTA Online</Heading2>
-      <Paragraph>
+      <Paragraph indent={false}>
         I&apos;ve been playing GTA Online for several years, and have
         {" "}
         accumulated a large wealth of virtual money and property. You can view
@@ -57,7 +58,7 @@ export default function Gaming () {
   );
 }
 
-Gaming.getView = function getView (page,) {
+Gaming.getView = function getView (page: ReactElement,) {
   return (
     <DefaultView>
       {page}
