60ab45ae4119f4db13d77cf907ea121717d4598c
Collin Bachman <collin@bachman.io>
2022-03-25 02:29:07 -0400

    Use Cover Image for OG Image if available in Blog Post

--- lib/seo/index.ts
+++ lib/seo/index.ts
@@ -56,11 +56,30 @@ export function getBlogPostSeo (post: ContentfulBlogPost,
     site: "@BachMacintosh",
     handle: "@BachMacintosh",
   };
+  const images = post.coverImage === null
+  || typeof post.coverImage === "undefined"
+    ? [
+      {
+        url: "https://images.ctfassets.net/kv526tbd0cl9/5dATXnPIZ9cP6EhlClNenX/bf3a70ca5dade5c4e36dc7c08d3f699a/discord_icon.png?w=256&h=256",
+        width: 256,
+        height: 256,
+        alt: "BachMacintosh.com",
+      },
+    ]
+    : [
+      {
+        url: post.coverImage.url,
+        width: 1280,
+        height: 320,
+        alt: post.coverImage.description,
+      },
+    ];
   const openGraph = {
     title: post.title,
     url: process.env.baseUrl + router.asPath,
     type: "article",
     article: { publishedTime: post.publishDate, modifiedTime: "", },
+    images,
   };
   if (post.coverImage === null) {
     twitter.cardType = "summary";
