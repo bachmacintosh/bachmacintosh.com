764e061f5c466d531cffae1ec862c8084bcd6309
Collin Bachman <collin@bachman.io>
2022-03-25 06:49:44 -0400

    Add Changelog Page

---
+++ pages/changelog.tsx
@@ -0,0 +1,40 @@
+import DefaultView from "../components/views/DefaultView";
+import { GetStaticProps, } from "next";
+import Markdown from "../components/layout/Markdown";
+import { NextSeo, } from "next-seo";
+import { ReactElement, } from "react";
+import { promises as fs, } from "fs";
+import { getPageSEO, } from "../lib/seo";
+import path from "path";
+import { useRouter, } from "next/router";
+
+type PageProps = {
+  changelogMarkdown: string,
+};
+
+
+export default function Changelog ({ changelogMarkdown, }: PageProps,) {
+  const title = "Changelog";
+  const description = "See the wonderful version history of this website.";
+  const router = useRouter();
+
+  return <>
+    <NextSeo {...getPageSEO(title, description, router,)} />
+    {/* eslint-disable-next-line react/no-children-prop */}
+    <Markdown children={changelogMarkdown} />
+  </>;
+}
+
+Changelog.getView = (page: ReactElement,) => {
+  return (
+    <DefaultView>
+      {page}
+    </DefaultView>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const changelogFile = path.join(process.cwd(), "CHANGELOG.md",);
+  const changelogMarkdown = await fs.readFile(changelogFile, "utf8",);
+  return { props: { changelogMarkdown, }, };
+};
