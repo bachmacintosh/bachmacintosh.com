8195aee33f65d87ef8e6d7048b5bbc6b5714c4f5
Collin Bachman <collin@bachman.io>
2022-03-26 18:47:47 -0400

    Add new view for pages with long-form content

---
+++ components/views/LongContentView.jsx
@@ -0,0 +1,23 @@
+import Footer from "../layout/Footer";
+import Navigation from "../layout/Navigation";
+import Warning from "../layout/Warning";
+
+export default function LongContentView ({ children, },) {
+  return (
+    <>
+      <Navigation />
+      <main className="mx-auto px-4 py-5 md:px-6 lg:px-8">
+        <div className="max-w-4xl mx-auto mt-16">
+          {process.env.isDeployPreview
+            && <Warning title="Deploy Preview">
+              This may contain test data, and is not indicative of the live site
+              .
+            </Warning>
+          }
+          {children}
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+}
