efb105dcb544004768fbd1c100227a640362a248
Collin Bachman <collin@bachman.io>
2022-04-28 14:16:19 -0400

    Remove Cypress, tests, and artifacts

--- cypress/integration/pages/index.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Home Page', () => {
-    it('should contain the site title', () => {
-        // Start from the index page
-        cy.visit('/');
-        cy.contains('BachMacintosh');
-        cy.get("#footer").contains("Copyright")
-    });
-});
