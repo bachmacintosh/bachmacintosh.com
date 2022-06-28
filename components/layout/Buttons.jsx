b516921c3dfce00a831daceb183a1c67aff4a3ee
Collin Bachman <collin@bachman.io>
2022-06-28 18:12:25 -0400

    Adjust button rounding

--- components/layout/Buttons.jsx
+++ components/layout/Buttons.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function ButtonLink ({ href, external, children, },) {
   const buttonClass = "mx-1 my-4 w-full inline-flex items-center "
     + "justify-center px-5 py-3 border border-transparent text-base "
-    + "font-medium rounded-md text-white bg-blue-ultra md:w-auto";
+    + "font-medium rounded-none text-white bg-blue-ultra md:w-auto";
   if (external === true) {
     return (
       <a className={buttonClass} type="button"
@@ -26,7 +26,7 @@ export function ButtonLink ({ href, external, children, },) {
 
 export function NsfwButton () {
   return (
-    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 md:w-auto`}>
+    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-none text-white bg-red-600 md:w-auto`}>
       Show NSFW Content
     </button>
   );
@@ -35,7 +35,7 @@ export function NsfwButton () {
 export function NsfwButtonLink ({ href, external, children, },) {
   const buttonClass = "mx-1 my-4 w-full inline-flex items-center "
     + "justify-center px-5 py-3 border border-transparent text-base "
-    + "font-medium rounded-md text-white bg-red-600 md:w-auto";
+    + "font-medium rounded-none text-white bg-red-600 md:w-auto";
   if (external === true) {
     return (
       <a className={buttonClass} type="button"
@@ -57,7 +57,7 @@ export function NsfwButtonLink ({ href, external, children, },) {
 
 export function SpoilerButton () {
   return (
-    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 md:w-auto`}>
+    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-none text-white bg-gray-600 md:w-auto`}>
       Show Spoiler Content
     </button>
   );
@@ -66,7 +66,7 @@ export function SpoilerButton () {
 export function SpoilerButtonLink ({ href, external, children, },) {
   const buttonClass = "mx-1 my-4 w-full inline-flex items-center "
     + "justify-center px-5 py-3 border border-transparent text-base "
-    + "font-medium rounded-md text-white bg-gray-600 md:w-auto";
+    + "font-medium rounded-none text-white bg-gray-600 md:w-auto";
   if (external === true) {
     return (
       <a className={buttonClass} type="button"
@@ -89,7 +89,7 @@ export function SpoilerButtonLink ({ href, external, children, },) {
 export function NsfwSpoilerButtonLink ({ href, external, children, },) {
   const buttonClass = "mx-1 my-4 w-full inline-flex items-center "
     + "justify-center px-5 py-3 border border-transparent text-base "
-    + "font-medium rounded-md text-white bg-red-400 md:w-auto";
+    + "font-medium rounded-none text-white bg-red-400 md:w-auto";
   if (external === true) {
     return (
       <a className={buttonClass} type="button"
@@ -111,7 +111,7 @@ export function NsfwSpoilerButtonLink ({ href, external, children, },) {
 
 export function NsfwSpoilerButton () {
   return (
-    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-400 md:w-auto`}
+    <button className={`mx-1 my-4 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-none text-white bg-red-400 md:w-auto`}
     >
       Show NSFW Spoiler Content
     </button>
